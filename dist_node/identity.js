19c2cfa37bc5b31dec60eadc1e4bd484b8b82360
Matt Bierner <mattbierner@gmail.com>
2014-03-17 22:16:45 -0400

    Cleaned up Identity

--- dist_node/identity.js
+++ dist_node/identity.js
@@ -2,7 +2,8 @@
  * THIS FILE IS AUTO GENERATED from 'lib/identity.kep'
  * DO NOT EDIT
 */"use strict";
-var Identity;
+var Monad = require("./monad"),
+    Identity;
 (Identity = (function(x) {
     var self = this;
     (self.value = x);
@@ -10,17 +11,9 @@ var Identity;
 (Identity.runIdentity = (function(c) {
     return c.value;
 }));
-var of = (function(x) {
+Monad(Identity, (function(x) {
     return new(Identity)(x);
-});
-(Identity.of = of);
-(Identity.prototype.of = of);
-var chain = (function(c, f) {
+}), (function(c, f) {
     return f(Identity.runIdentity(c));
-});
-(Identity.chain = chain);
-(Identity.prototype.chain = (function(f) {
-    var self = this;
-    return chain(self, f);
 }));
 (module.exports = Identity);
