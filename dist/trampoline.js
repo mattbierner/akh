cd9ecb8b09746eea16906123e481025bc238ce56
Matt Bierner <mattbierner@gmail.com>
2014-03-24 14:17:27 -0400

    Recompile

--- dist/trampoline.js
+++ dist/trampoline.js
@@ -5,7 +5,7 @@
 define(["require", "exports", "./structure"], (function(require, exports, __o) {
     "use strict";
     var Monad = __o["Monad"],
-        Trampoline;
+        Trampoline, x;
     (Trampoline = (function() {}));
     var Done = (function(x) {
         var self = this;
@@ -22,7 +22,6 @@ define(["require", "exports", "./structure"], (function(require, exports, __o) {
         var self = this;
         (self.c = c);
         (self.f = f);
-        if ((!(((c instanceof Chain) || (c instanceof Thunk)) || (c instanceof Done)))) debugger;
     });
     (Chain.prototype = new(Trampoline)());
     var Ap = (function(c, f) {
@@ -33,9 +32,9 @@ define(["require", "exports", "./structure"], (function(require, exports, __o) {
         appk = (function(f, x) {
             return ((f instanceof Ap) ? new(Chain)(f.c(x), f.f) : f(x));
         });
-    Monad(Trampoline, (function(x) {
-        return new(Done)(x);
-    }), (function(c, f) {
+    Monad(Trampoline, ((x = Done), (function(y) {
+        return new(x)(y);
+    })), (function(c, f) {
         return (((c instanceof Chain) && false) ? new(Chain)(c.c, (function(x) {
             return c.f(x)
                 .chain(f);
